77123eebb206d612bd394edbe2071539c2fac8d3
tonyreiz <anthonyreisa200@gmail.com>
2024-06-25 15:26:20 -0300

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,36 @@
+// alert("Olá Mundo");
+// alert("Me livrei da maldição");
+
+// let  nome = "Anthony Reis dos Santos"
+// alert(nome);
+// nome = "Vixi moiou pra nóis!!!"
+// alert(nome);
+let nome = prompt("Digite Seu Nome");
+
+
+let = idade =prompt("Digite Sua Idade")
+
+
+let sexualidade = prompt("Qual Sua Sexualidade")
+
+
+let cor = prompt("Digite Sua Cor Favorita")
+
+
+
+let filme = prompt("Digite Seu Filme Favorito ")
+
+
+
+let comida = prompt("Digite Sua Comida Favorita")
+
+
+let Série = prompt("Digite Sua Série Favorita")
+
+
+alert(`Boa tarde " + ${nome} +" Sua idade é  " + ${idade} + " Seu sexo é " + ${sexualidade} + " Sua cor favorita é " + ${cor} + "  Seu filme favorito é " + ${filme} + " Sua comida favorita é " + ${comida} + " Sua serie favorita é + ${Série} `);
+
+
+
+
+// prompt("Digite Seu Nome.")
